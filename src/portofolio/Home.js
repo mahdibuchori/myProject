8f3941895a6b090840db2d3eb0c59b5960aea7b8
mahdibuchori <mahdibuchori@gmail.com>
2023-05-02 08:48:31 +0700

    text run

--- src/portofolio/Home.js
+++ src/portofolio/Home.js
@@ -11,7 +11,7 @@ export const Home = () => {
   const [isDeleting, setIsDeleting] = useState(false);
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() * 100);
-  const [setIndex] = useState(1);
+  const [index, setIndex] = useState(1);
   const toRotate = [ "GOOD FOOD GOOD TASTE" ];
   const period = 2000;
   
@@ -21,11 +21,11 @@ export const Home = () => {
     }, delta);
   
     return () => { clearInterval(ticker) };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [text])
   
   const tick = () => {
-      
+    let baca = index
+    console.log(baca)
       let i = loopNum % toRotate.length;
       let fullText = toRotate[i];
       let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
