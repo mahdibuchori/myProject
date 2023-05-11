190b3c4abb2f2d0ac8ffd4b878ae9bbf2e9c5e90
mahdibuchori <mahdibuchori@gmail.com>
2023-05-11 14:48:37 +0700

    set envs

--- src/apis/apisData.js
+++ src/apis/apisData.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
-import env from "react-dotenv";
-const urlAuth = `${env.REACT_APP_API_KEY_YT}`;
-const urlGsheet = `${env.REACT_APP_API_KEY_GG}`
+const urlAuth = `${process.env.REACT_APP_API_KEY_YT}`;
+const urlGsheet = `${process.env.REACT_APP_API_KEY_GG}`
 
 export const API_AUTH = axios.create({
     baseURL: urlAuth,
